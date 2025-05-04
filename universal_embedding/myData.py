3e2597f2683490e487ca9470ceb8b0b0d850d7f9
SeanCheng1996 <sc159@rice.edu>
2025-05-03 21:35:34 -0500

    update universal_(dataset, model, train)

--- universal_embedding/myData.py
+++ universal_embedding/myData.py
@@ -1,20 +1,36 @@
 import torch
 from torch.utils.data import Dataset
 from pandas import DataFrame
+import numpy as np
 
 
 class GraphDataset(Dataset):
     def __init__(self, link_table: DataFrame, model):
         """
 
-        :param link_table: dataframe. index, u, v, t, linked
+        :param link_table: dataframe. index, u_id, v_id, t, linked  [10, 11, 2000, 1]
         :param model:
         """
         self.link_table = link_table
         self.model = model
 
-    def get_embedding(self, node, t):
-        return torch.rand(128)
+    def get_neighbors(self, node, G, t=1000, k=5):
+        return [10, 11]
+
+    def get_embedding(self, u: int, timestamp: float, lambda_decay: float = 1.0, neighbors: list = None):
+        hu_list = []
+        model = self.model
+        for v in neighbors:
+            src_node_ids = np.array([u])
+            dst_node_ids = np.array([v])
+            node_interact_times = np.array([timestamp])
+            hu, _ = model.compute_src_dst_node_temporal_embeddings(src_node_ids, dst_node_ids, node_interact_times)
+            hu_list.append(hu.squeeze(0))
+        hu_tensor = torch.stack(hu_list, dim=0)  # [k, d]
+        positions = torch.arange(len(neighbors)).float()
+        weights = torch.softmax(-lambda_decay * positions, dim=0)  # shape: [k]
+        hu_avg = torch.sum(weights.unsqueeze(1) * hu_tensor, dim=0)  # shape: [d]
+        return hu_avg
 
     def __getitem__(self, index):
         # get u,v,t, linked from link_table
